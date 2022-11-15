c107bf136ce32e99a7d3cdeab2013e86679980ba
John Aughey <jha@aughey.com>
2022-11-15 14:19:20 +0000

    works

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,22 +2,72 @@ import React from 'react';
 import './App.css';
 import { ShowGraph } from './GraphView/ShowGraph';
 
+interface Port {
+  Kind: string;
+  Name: string;
+}
+interface Node {
+  Kind: string;
+  Inputs: Port[];
+  Outputs: Port[];
+}
+interface Connection {
+  From: string;
+  Output: string;
+  To: string;
+  Input: string;
+}
+interface NodeMap {
+  [key: string]: Node;
+}
+interface ConnectionMap {
+  [key: string]: Connection;
+}
+interface Graph {
+  Nodes: NodeMap,
+  Connections: ConnectionMap
+}
+
 function App() {
-  const [data, setData] = React.useState<string | null>(null);
+  const [graph,setGraph] = React.useState<Graph | null>(null);
+
+  const createGraph = (data: any) => {
+    console.log(data);
+
+    const Nodes: NodeMap = {};
+    for (let n of data.Nodes) {
+      Nodes[n.Id] = {
+        Kind: n.Name,
+        Inputs: n.Inputs,
+        Outputs: n.Outputs
+      };
+    }
+    const Connections: ConnectionMap = {};
+    let connectionid = 1;
+    for (let c of data.Connections) {
+      Connections[connectionid++] = {
+        From: c.from,
+        Output: c.outputname,
+        To: c.to,
+        Input: c.inputname
+      };
+    }
+  
+    const graph = {
+      Nodes: Nodes,
+      Connections: Connections
+    }
+    setGraph(graph);
+  }  
 
   React.useEffect(() => {
     fetch('/newsies.ive')
       .then((response) => response.json())
-      .then((data) => setData(data));
-  },[]);
+      .then((data) => createGraph(data));
+  }, []);
 
-  if(!data) {
+  if (!graph) {
     return null;
-  }
-
-  const graph = {
-    Nodes:{},
-    Connections: {}
   }
 
   return (
--- app/src/GraphView/ShowGraph.js
+++ app/src/GraphView/ShowGraph.js
@@ -18,12 +18,43 @@ export function ShowGraph({ graph }) {
     return "ID" + i.replace(/-/g, '');
   }
 
-  var nodes = Object.keys(graph.Nodes).map(k => `${id(k)} [label="${graph.Nodes[k].Kind}"];`);
-  var connections = Object.keys(graph.Connections).map(k => `${id(graph.Connections[k].From)} -> ${id(graph.Connections[k].To)};`);
+  function nodetable(n) {
+    const inputs = n.Inputs;
+    const outputs = n.Outputs;
+    const portlength = Math.max(inputs.length, outputs.length);
+
+    const preamble = `<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+      <TR><TD COLSPAN="2">${n.Kind}</TD></TR>`
+      //<TR><TD>Inputs</TD><TD>Outputs</TD></TR>`
+
+    const ports = [];
+    for(let i = 0; i < portlength; i++) {
+      ports.push(`<TR><TD PORT="i${i}">${inputs[i]?.Name || ''}</TD><TD PORT="o${i}">${outputs[i]?.Name || ''}</TD></TR>`);
+    }
+
+    const postamble = `</TABLE>`;
+    return preamble + ports.join('') + postamble;
+  }
+
+  function portIndex(ports, name) {
+    return ports.findIndex(p => p.Name === name);
+  }
+
+  function connection(c) {
+    const from = graph.Nodes[c.From];
+    const to = graph.Nodes[c.To];
+    const srcindex = portIndex(from.Outputs, c.Output);
+    const dstindex = portIndex(to.Inputs, c.Input);
+    return `${id(c.From)}:o${srcindex} -> ${id(c.To)}:i${dstindex};`
+  }
+
+  var nodes = Object.keys(graph.Nodes).map(k => `${id(k)} [label=<${nodetable(graph.Nodes[k])}>];`);
+  var connections = Object.keys(graph.Connections).map(k => connection(graph.Connections[k]));
 
   const dot = `
 digraph{
   rankdir=LR;
+  node [shape=plaintext];
   ${nodes.join("\n")}
   ${connections.join("\n")}
 }`;
@@ -32,12 +63,16 @@ digraph{
 
   return (
     <div style={{ width: '100%' }}>
-      <Graphviz className="graphviz" width="100%" dot={dot} />
+      <script src="https://unpkg.com/@hpcc-js/wasm/dist/index.min.js" type="application/javascript/"></script>
+
+
+
+      <Graphviz className="graphviz" options={{ width: '100%', zoom: true, useWorker: false }} dot={dot} />
       String Graph: <input type="checkbox" checked={stripGraph} onChange={e => setStripGraph(e.target.checked)} />
       Double Quote: <input type="checkbox" checked={doubleQuote} onChange={e => setDoubleQuote(e.target.checked)} />
-      <pre>
+      {/* <pre>
         {graphstring}
-      </pre>
+      </pre> */}
     </div>
   )
 }
